264511dfb6abb58a9028602eeb9d78a589fe2210
Edwin Chiu <echiu2@buffalo.edu>
2020-07-25 16:07:03 -0400

    added css styling to home

--- frontend/components/Home.js
+++ frontend/components/Home.js
@@ -1,19 +1,43 @@
 import React, { useState, useEffect } from "react";
-import { connect } from 'react-redux'
+import { connect } from "react-redux";
 
-const Home = ({post}) => {
+const Home = ({ post }) => {
   // console.log('post', post)
   // const [caption, setCaption] = useState(`${post.caption}`);
-  console.log('post', post)
-  return post.length > 0 ?(
-    <div>
-        <h1>Home Page!</h1>
-        {post.map((option) => (
-          <p>{option.caption}</p>
-        ))}
+  console.log("post", post);
+  return post.length > 0 ? (
+    <div class="container-fluid gedf-wrapper">
+      <h1>
+        <center>Newsfeed</center>
+      </h1>
+      <div class="row">
+        <div class="col-md-3"></div>
+        <div class="col-md-6 gedf-main">
+          {post.map((option) => (
+            <div class="card">
+              <div class="card-body">
+                <p class="card-text">{option.caption}</p>
+              </div>
+              <div class="card-footer">
+                <a href="#" class="card-link">
+                  <i class="fa fa-gittip"></i> Like
+                </a>
+                <a href="#" class="card-link">
+                  <i class="fa fa-comment"></i> Comment
+                </a>
+                <a href="#" class="card-link">
+                  <i class="fa fa-mail-forward"></i> Share
+                </a>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
-  ) : <h1> No Post! </h1>
-}
+  ) : (
+    <h1> No Post! </h1>
+  );
+};
 //connect to store
 //grab posts from store
 //map through props to display each post
@@ -23,3 +47,9 @@ const mapstate = ({ post }) => ({
 });
 
 export default connect(mapstate, null)(Home);
+{
+  /* <h1>Home Page!</h1>
+{post.map((option) => (
+  <h1>{option.caption}</h1>
+))} */
+}
