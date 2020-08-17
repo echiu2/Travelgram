199dbe5b3e1252721c2c4ad3b1366eb6eb269beb
Edwin Chiu <echiu2@buffalo.edu>
2020-08-17 18:14:49 -0400

    jwt setup

---
+++ frontend/redux/authentication.js
@@ -0,0 +1,29 @@
+
+
+// There are three possible states for our login
+// process and we need actions for each of them
+export const LOGIN_REQUEST = 'LOGIN_REQUEST'
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS'
+export const LOGIN_FAILURE = 'LOGIN_FAILURE'
+
+const _requestLogin = (creds) => ({
+    type: LOGIN_REQUEST,
+    isFetching: true,
+    isAuthenticated: false,
+    creds
+})
+
+const _receiveLogin = (user) => ({
+    type: LOGIN_SUCCESS,
+    isFetching: false,
+    isAuthenticated: true,
+    id_token: user.id_token
+})
+
+const _loginError = (message) => ({
+    type: LOGIN_FAILURE,
+    isFetching: false,
+    isAuthenticated: false,
+    message
+})
+
